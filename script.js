6034a93020644613a372cb4d7abed3db6a9335c0
Mariia Butolina <104693230+butolinka@users.noreply.github.com>
2023-01-06 20:29:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function setDate(){
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees= ((seconds/60)*360)+90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; 
     
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
